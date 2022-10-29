d650f45cfc84d47280b274fc1b948dfb29a13095
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-10-29 19:05:18 -0400

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(input) {
+
+    let output = "";
+
+    for(let i = input.length-1; i >=0; i--) {
+        output += input[i];
+    }
+
+    return output;
+
 
 };
 
