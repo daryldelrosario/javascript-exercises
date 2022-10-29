fc16588fc65c144a78f08ce3bffb4650a4fe1c77
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-10-29 19:37:49 -0400

    Pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    let sum = 0;
+
+    if(!Number.isInteger(one) || !Number.isInteger(two)) {
+        return "ERROR";
+    }
+
+    if(one > two) {
+        newOne = two;
+        newTwo = one;
+    } else if (one > 0 && two > 0 && one < two) {
+        newOne = one;
+        newTwo = two;
+    } else if (one < 0 || two < 0) {
+        return "ERROR";
+    } else {
+        return "ERROR";
+    }
+
+    for(let i = newOne; i <= newTwo; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
